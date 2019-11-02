263b8a1503cd16c34171f77a4b17ce63d50b4e54
Vianney Traina <vianney.traina@edu.devinci.fr>
2019-11-02 01:00:48 +0100

    Add fetch api

--- my_pokedex_app/src/pages/Pokedex.js
+++ my_pokedex_app/src/pages/Pokedex.js
@@ -6,23 +6,36 @@ import Filtre from '../components/Filtre';
 class Pokedex extends Component {
     state = {
         value: '',
+        api: []
     };
 
-    handleSubmit = (ev) => {
+    handleSubmit = async (ev) => {
         ev.preventDefault();
         const { value } = ev.currentTarget.filter;
         console.log(new FormData(ev.currentTarget).get('filter'));
 
-        this.setState({
-            value,
-        });
+        try {
+            
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+        const data = await response.json();
+        this.setState({ api: data });
+        console.log(this.state.api);
+
+        } catch (err) {
+            console.log(err);
+
+            throw err;
+        } 
     }
 
     render() {
+        const { data } = this.state;
+
         return(
             <div className="App">
                 <h1>Hello World !</h1>
                 <Filtre value={this.state.value} handleSubmit={this.handleSubmit} />
+                {<p>{data}</p>}
             </div>
         );
     }
